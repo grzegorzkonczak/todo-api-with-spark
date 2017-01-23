b2b36a5be5f3b6840177fb8e958e31038e94e273
Grzegorz Kończak <grzegorz.konczak@gmail.com>
2017-01-23 20:52:22 +0100

    Tests for updating and retrieving all todos added.  Also one additional test for creating todo added.

--- src/test/java/com/checkrise/todos/ApiTest.java
+++ src/test/java/com/checkrise/todos/ApiTest.java
@@ -4,6 +4,7 @@ package com.checkrise.todos;
 import com.checkrise.testing.ApiClient;
 import com.checkrise.testing.ApiResponse;
 import com.checkrise.todos.dao.Sql2oTodoDao;
+import com.checkrise.todos.model.Todo;
 import com.google.gson.Gson;
 import org.junit.*;
 import org.sql2o.Connection;
@@ -14,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ApiTest {
     public static final String PORT = "4568";
@@ -56,5 +58,60 @@ public class ApiTest {
         ApiResponse response = client.request("POST", "/api/v1/todos", gson.toJson(values));
 
         assertEquals(201, response.getStatus());
+    }
+
+    @Test
+    public void addingTodoCreatesItInDatabase() throws Exception {
+        Map<String, Object> values = new HashMap<>();
+        values.put("name", "Test");
+        values.put("completed", true);
+
+        ApiResponse response = client.request("POST", "/api/v1/todos", gson.toJson(values));
+        Todo createdTodo = gson.fromJson(response.getBody(), Todo.class);
+
+
+        assertEquals(createdTodo, dao.findById(createdTodo.getId()));
+    }
+
+    @Test
+    public void whenMultipleTodosPresentReturnsAll() throws Exception {
+        dao.create(new Todo("Test1", true));
+        dao.create(new Todo("Test2", true));
+
+        ApiResponse response = client.request("GET", "/api/v1/todos");
+        Todo[] todos = gson.fromJson(response.getBody(), Todo[].class);
+
+        assertEquals(2, todos.length);
+    }
+
+    @Test
+    public void whenTodoUpdatedReturnsAcceptedStatus() throws Exception {
+        Todo todo = newTestTodo();
+        dao.create(todo);
+        todo.setName("New Name");
+
+        ApiResponse response = client.request("PUT", "/api/v1/todos/" + todo.getId(),
+                gson.toJson(todo));
+
+        assertEquals(202, response.getStatus());
+    }
+
+    @Test
+    public void whenTodoUpdatedDataInTodoChanges() throws Exception {
+        Todo todo = newTestTodo();
+        dao.create(todo);
+        String newName = "New Name";
+        todo.setName(newName);
+
+        client.request("PUT", "/api/v1/todos/" + todo.getId(),
+                gson.toJson(todo));
+
+        assertEquals(newName, dao.findById(todo.getId()).getName());
+    }
+
+
+
+    private Todo newTestTodo() {
+        return new Todo("Test", true);
     }
 }
